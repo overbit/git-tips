73f143bab91bd8de4b34ac853d322fe1d4ee8197
Daniele Iasella <2861984+overbit@users.noreply.github.com>
2020-06-05 15:51:07 +0100

    Add script to generate a clone-all repos based on repos in a folder

---
+++ generate-git-clone-all.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+dirs=$(find . -name '.git' -type d | sed 's/\/\.git//')
+
+for dir in $dirs; do
+  GIT_DIR=$dir/.git
+
+  # Creates the folder structure
+  if [[ -n $(git --git-dir=$GIT_DIR remote) ]]; then
+    echo mkdir -p $dir;
+  fi
+
+  # Clones the repository in the same folder
+  COMMAND=$(git --git-dir=$GIT_DIR remote | grep -n origin)
+  if [[ -n $COMMAND ]]; then
+    echo git clone $(git --git-dir=$GIT_DIR remote get-url origin) $dir;
+  fi
+
+  # Re-adds other remotes
+  for r in $(git --git-dir=$GIT_DIR remote | grep -v origin); do
+    echo git --git-dir=$GIT_DIR remote add $r $(git --git-dir=$GIT_DIR remote get-url $r);
+  done
+
+  echo
+done > clone-all.sh
